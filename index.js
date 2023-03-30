fd6c016614e2c3cb760f47b8797b17e263154c1d
Brooklyn <85711322+brooklyn5991@users.noreply.github.com>
2023-03-29 22:59:36 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,107 @@
+var firstDrum = new Audio('sounds/tom-1.mp3' );
+var secondDrum = new Audio('sounds/tom-2.mp3' );
+var thirdDrum = new Audio('sounds/tom-3.mp3' );
+var fourthDrum = new Audio('sounds/tom-4.mp3' );
+var snareDrum = new Audio('sounds/snare.mp3' );
+var crashDrum = new Audio('sounds/crash.mp3' );
+var bassDrum = new Audio('sounds/kick-bass.mp3' );
+
+function w() { 
+    firstDrum.play(); 
+  } 
+  
+function a() { 
+    secondDrum.play(); 
+  } 
+
+  function s() { 
+    thirdDrum.play(); 
+  } 
+
+  function d() { 
+    fourthDrum.play(); 
+  } 
+
+  function j() { 
+    snareDrum.play(); 
+  } 
+
+  function k() { 
+    crashDrum.play(); 
+  } 
+
+  function l() { 
+    bassDrum.play(); 
+  } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var numberOfDrumKits = document.querySelectorAll(".drum").length; 
+// var soundArray = array['sounds/crash.mp3', 'sounds/kick-bass.mp3', 'sounds/snare.mp3',
+// 'sounds/tom-1.mp3', 'sounds/tom-2.mp3', 'sounds/tom-3.mp3', 'sounds/tom-.mp3' ]
+
+
+// for (var i = 0; i < numberOfDrumKits; i++) {
+    // document.querySelector("#first").addEventListener("click", function () {
+    // var audio = new Audio('sounds/tom-1.mp3' );
+    // audio.play();
+
+       
+
+    // function () {
+        
+    // }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+// });
+
+
+
+
+// }
+
+
+
+
+ // this.style.color = '#000000';
